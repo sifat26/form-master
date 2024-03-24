dad1fc14f87849b895da4ce5d133e5e9f253e753
sifat26 <sifatict26@gmail.com>
2024-03-24 23:03:57 +0600

    form update

--- src/SimpleForm/SimpleForm.jsx
+++ src/SimpleForm/SimpleForm.jsx
@@ -1,15 +1,23 @@
 const SimpleForm = () => {
     const handleSubmit = (e) => {
-        console.log(e);
+        e.preventDefault();
+        console.log("form submitted");
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
 
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" />
+                <input type="text" name="name" />
                 <br />
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="phone" name="phone" /> <br />
                 <input type="submit" value="Submit" />
+               
             </form>
             
         </div>
